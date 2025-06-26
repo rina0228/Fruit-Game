049fdb1eecfa233cd1de01d1be0ec6f6b18a71b1
Riuu <mukhiyarina213@gmail.com>
2025-06-26 12:37:45 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,24 @@
 self.addEventListener("install", function (event) {
-  console.log("Service Worker Installed");
+  console.log("Service Worker installed!");
+  event.waitUntil(
+    caches.open("fruit-game-cache").then(function (cache) {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./fruit.css",
+        "./fruit.js",
+        "./icon.png",
+        "./bg-music.mp3",
+        "./manifest.json"
+      ]);
+    })
+  );
 });
 
 self.addEventListener("fetch", function (event) {
-  // Offline mode ke liye future mein enhance kar sakte ho
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
 });
